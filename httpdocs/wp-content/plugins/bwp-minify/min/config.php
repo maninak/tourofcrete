f85214411d3be6c8395bd3524a2c4b6aedd32e74
Kostis Maninakis -serverside <kmaninak@gmail.com>
2016-01-17 15:43:51 +0000

    Updates with changes in script load order

--- httpdocs/wp-content/plugins/bwp-minify/min/config.php
+++ httpdocs/wp-content/plugins/bwp-minify/min/config.php
@@ -7,10 +7,10 @@ $min_cachePath = dirname(dirname(__FILE__)) . '/cache';
 $min_documentRoot = '';
 $min_cacheFileLocking = true;
 $min_serveOptions['bubbleCssImports'] = true;
-$min_serveOptions['maxAge'] = 2592000;
+$min_serveOptions['maxAge'] = 7776000;
 $min_serveOptions['minApp']['groupsOnly'] = false;
 $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2015-12-02 01:32:00
+// auto-generated on 2015-12-30 14:49:49
