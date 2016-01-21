fa05e72f1d410ba19e6767a2e70016009be72722
Kostis Maninakis <kmaninak@gmail.com>
2016-01-22 00:41:39 +0200

    Adds Facebook analytics (pixel)

--- httpdocs/wp-content/themes/tourofcrete/functions.php
+++ httpdocs/wp-content/themes/tourofcrete/functions.php
@@ -110,6 +110,34 @@ function custom_header() {
 
 
 
+
+<?php
+	// Adds facebook analytics tracking -K
+	add_action('wp_footer', 'add_facebookanalytics');
+	function add_facebookanalytics() { 
+?>
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '461713000682562');
+		fbq('track', "PageView");
+	</script>
+	<noscript>
+		<img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=461713000682562&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
+<?php 
+	} 
+?>
+
+
+
 <?php 
 }
 
