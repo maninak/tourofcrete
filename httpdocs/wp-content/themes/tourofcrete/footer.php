32247a997ef9423ecd4bf17f82e6464f2b24a06f
Kostis Maninakis <kmaninak@gmail.com>
2016-03-23 19:45:38 +0200

    Adds more sponsors to the footer

--- httpdocs/wp-content/themes/tourofcrete/footer.php
+++ httpdocs/wp-content/themes/tourofcrete/footer.php
@@ -37,11 +37,26 @@ echo '</div>'; //* end .site-container or #wrap
             <a href="http://www.hellenic-cycling.gr/" target="_blank" alt="ΕΟΠ">
                <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/EOP.png" title="Hellenic Cycling Association" border="0" />
             </a>
+            <a href="http://www.garmin.com/en-US" target="_blank" alt="Garmin">
+               <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/garmin.png" title="Garmin" border="0" />
+            </a>
             <a href="http://en.aegeanair.com/" target="_blank" alt="Aegean Airlines">
                <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/aegean_white.png" title="Aegean Airlines" border="0" />
             </a>
             <a href="http://www.minoan.gr/en/crete" target="_blank" alt="Minoan Lines">
                <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/minoan.png" title="Minoan Lines" border="0" />
+            </a>
+            <a href="http://www.armodios.com/" target="_blank" alt="Armodios Embroidery">
+               <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/armodios.png" title="Armodios Embroidery" border="0" />
+            </a>
+            <a href="" target="_blank" alt="i*mad">
+               <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/imad.png" title="i*mad" border="0" />
+            </a>
+            <a href="http://www.portoelounda.com/en/home" target="_blank" alt="Porto Elounda">
+               <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/porto_elounda.png" title="Porto Elounda" border="0" />
+            </a>
+            <a href="http://www.haibike.de/" target="_blank" alt="HAIBIKE">
+               <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/haibike.png" title="HAIBIKE" border="0" />
             </a>
             <a href="http://www.yamahamoraitis.gr/" target="_blank" alt="i-Moto Yamaha Moraitis">
                <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/i-moto.png" title="i-Moto Yamaha Moraitis" border="0" />
@@ -57,6 +72,9 @@ echo '</div>'; //* end .site-container or #wrap
             </a>
             <a href="https://www.facebook.com/Delta-Carbon-Works-785460848159497" target="_blank" alt="Cyclomania" target="_blank" alt="Cyclomania">
                <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/cyclomania.png" title="Cyclomania" border="0" />
+            </a>
+            <a href="http://www.mindev.gov.gr/" target="_blank" alt="Hellenic Ministry of Economy Development and Tourism">
+               <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/.png" title="Hellenic Ministry of Economy Development and Tourism" border="0" />
             </a>
             <a href="http://www.ruraltourismincrete.com/en" target="_blank" alt="Rural Tourism and Agrotourism Union of Crete">
                <img src="http://tourofcrete.com/wp-content/themes/tourofcrete/images/agro-union.png" title="Rural Tourism and Agrotourism Union of Crete" border="0" />
