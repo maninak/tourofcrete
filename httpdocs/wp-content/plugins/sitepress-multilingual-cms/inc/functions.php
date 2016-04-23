dbd5e27950914107665514e1691577cec474a7cd
Kostis Maninakis -serverside <kmaninak@gmail.com>
2016-04-23 23:26:51 +0000

    Fixes WPML plugin translated nav-bar menu creation bug

--- httpdocs/wp-content/plugins/sitepress-multilingual-cms/inc/functions.php
+++ httpdocs/wp-content/plugins/sitepress-multilingual-cms/inc/functions.php
@@ -188,3 +188,19 @@ function icl_makes_duplicates( $master_post_id )
 		$iclTranslationManagement->make_duplicates_all( $master_post_id );
 	}
 }
+
+function wpml_prepare_in( $items, $format = '%s' ) {
+ global $wpdb;
+
+ $items    = (array) $items;
+ $how_many = count( $items );
+ if ( $how_many > 0 ) {
+  $placeholders    = array_fill( 0, $how_many, $format );
+  $prepared_format = implode( ",", $placeholders );
+  $prepared_in     = $wpdb->prepare( $prepared_format, $items );
+ } else {
+  $prepared_in = "";
+ }
+
+ return $prepared_in;
+}
